8373c3c750ed8848666120a9088437ebc28883d5
thtmmy <mibarriolosteques@gmail.com>
2024-12-09 20:03:38 +0100

    settings.py

--- charity/settings.py
+++ charity/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m$r-it!dcb--2vn87ub&_745u30%z2s^y=m)!+34nagms)resi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
